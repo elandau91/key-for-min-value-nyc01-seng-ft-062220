316a08917e73c0115939788c72960ea4217222cc
Eli Landau <eli.b.landau@gmail.com>
2020-06-09 15:00:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,19 @@ def key_for_min_value(name_hash)
   low = 50000
   low_man = ""
   name_hash.each do |key, value|
-    if name_hash == {}
-      low_man = false
-    end  
     if value < low
       low = value
     end
+    
     if value == low
       low_man = key
     end
+    
+    if name_hash == {}
+      low_man = false
+    end  
   end
+  
   low_man
 end
 
