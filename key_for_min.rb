c82698829c635eb8a2465bb85fe4e163afbb5087
Eli Landau <eli.b.landau@gmail.com>
2020-06-09 14:46:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ 
+  low = 50000
+  low_man = ""
+  name_hash.each do |key, value|
+    if value < low
+      low = value
+    end
+    if value == low
+      low_man = key
+    end
+    if name_hash == {}
+      low_man = "nil"
+    end  
+  end
+  low_man
 end
+
